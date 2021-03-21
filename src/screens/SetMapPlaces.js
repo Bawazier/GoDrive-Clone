82f52dcc905aabbf127ef839a02c614b00c2e730
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-03-22 00:21:46 +0700

    add FooterDestination and FooterOrigin to SetMapPlaces screen

--- src/screens/SetMapPlaces.js
+++ src/screens/SetMapPlaces.js
@@ -1,13 +1,65 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
-import {Container, Content, H1} from 'native-base';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import {Container, Content, H1, Footer, Icon} from 'native-base';
+import FooterDestination from '../components/FooterDestination';
+import FooterOrigin from '../components/FooterOrigin';
 
-function SetMapPlaces() {
+function SetMapPlaces({navigation}) {
   return (
-    <Container>
-      <Content style={styles.baseText}>
+    <Container style={{backgroundColor: 'green'}}>
+      <Content>
         <H1>Maps</H1>
       </Content>
+      <Footer
+        style={{
+          backgroundColor: 'transparent',
+          elevation: 0,
+          height: 'auto',
+          flexDirection: 'column',
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginBottom: 10,
+            paddingHorizontal: 10,
+          }}>
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            style={{
+              backgroundColor: 'white',
+              width: 50,
+              height: 50,
+              borderRadius: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Icon
+              name="arrow-left"
+              type="FontAwesome5"
+              style={{fontSize: 18, color: 'black'}}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              backgroundColor: 'white',
+              width: 50,
+              height: 50,
+              borderRadius: 50,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Icon
+              name="map-pin"
+              type="FontAwesome5"
+              style={{fontSize: 18, color: 'black'}}
+            />
+          </TouchableOpacity>
+        </View>
+        {/* <FooterDestination /> */}
+        <FooterOrigin />
+      </Footer>
     </Container>
   );
 }
