c04c87f7c3cfa1d034b57f27246ee046bffa4157
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-03-22 22:41:46 +0700

    add ModalFeedback to ConfirmOrder screen

--- src/screens/ConfirmOrder.js
+++ src/screens/ConfirmOrder.js
@@ -1,92 +1,112 @@
 /* eslint-disable react-native/no-inline-styles */
 import React, {useCallback, useMemo, useRef} from 'react';
 import {StyleSheet, View, TouchableOpacity} from 'react-native';
-import {Container, Footer, Content, Icon} from 'native-base';
+import {Container, Footer, Content, Icon, Text} from 'native-base';
 import HeaderEdit from '../components/HeaderEdit';
-import BottomSheet from '@gorhom/bottom-sheet';
+import BottomSheet, {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
 import FooterCompleted from '../components/FooterCompleted';
 import FooterOrder from '../components/FooterOrder';
 import CardTransport from '../components/CardTransport';
+import ModalFeedback from '../components/ModalFeedback';
 
 function ConfirmOrder({navigation}) {
   // ref
   const bottomSheetRef = useRef(null);
+  const bottomSheetModalRef = useRef(null);
 
   // variables
-  const snapPoints = useMemo(() => ['50%'], []);
+  const snapPoints = useMemo(() => ['40%'], []);
+  const snapSheetModalPoints = useMemo(() => ['100%'], []);
 
   // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
   const handleSheetChanges = useCallback(index => {
     console.log('handleSheetChanges', index);
   }, []);
 
   return (
-    <Container style={{backgroundColor: 'green'}}>
-      <HeaderEdit />
-      <Content />
-      <View style={{flex: 3}}>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            marginBottom: 10,
-            paddingHorizontal: 10,
-            flex: 1,
-          }}>
-          <TouchableOpacity
-            onPress={() => navigation.goBack()}
+    <BottomSheetModalProvider>
+      <Container style={{backgroundColor: 'green'}}>
+        <HeaderEdit />
+        <Content />
+        <View style={{flex: 3}}>
+          <View
             style={{
-              backgroundColor: 'white',
-              width: 50,
-              height: 50,
-              borderRadius: 50,
+              flexDirection: 'row',
               alignItems: 'center',
-              justifyContent: 'center',
+              justifyContent: 'space-between',
+              marginBottom: 10,
+              paddingHorizontal: 10,
+              flex: 1,
             }}>
-            <Icon
-              name="arrow-left"
-              type="FontAwesome5"
-              style={{fontSize: 18, color: 'black'}}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              backgroundColor: 'white',
-              width: 50,
-              height: 50,
-              borderRadius: 50,
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <Icon
-              name="map-pin"
-              type="FontAwesome5"
-              style={{fontSize: 18, color: 'black'}}
-            />
-          </TouchableOpacity>
-        </View>
-        <BottomSheet
-          ref={bottomSheetRef}
-          index={0}
-          snapPoints={snapPoints}
-          onChange={handleSheetChanges}>
-          <View style={{flexDirection: 'column'}}>
-            <CardTransport />
-            <CardTransport />
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{
+                backgroundColor: 'white',
+                width: 50,
+                height: 50,
+                borderRadius: 50,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Icon
+                name="arrow-left"
+                type="FontAwesome5"
+                style={{fontSize: 18, color: 'black'}}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                backgroundColor: 'white',
+                width: 50,
+                height: 50,
+                borderRadius: 50,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Icon
+                name="map-pin"
+                type="FontAwesome5"
+                style={{fontSize: 18, color: 'black'}}
+              />
+            </TouchableOpacity>
           </View>
-        </BottomSheet>
-      </View>
-      <Footer
-        style={{
-          backgroundColor: 'transparent',
-          elevation: 0,
-          height: 'auto',
-          flexDirection: 'column',
-        }}>
-        <FooterOrder />
-      </Footer>
-    </Container>
+          <BottomSheet
+            ref={bottomSheetRef}
+            index={0}
+            snapPoints={snapPoints}
+            onChange={handleSheetChanges}>
+            <View style={{flexDirection: 'column'}}>
+              <CardTransport />
+              <CardTransport />
+            </View>
+          </BottomSheet>
+        </View>
+        <Footer
+          style={{
+            backgroundColor: 'transparent',
+            elevation: 0,
+            height: 'auto',
+            flexDirection: 'column',
+          }}>
+          {/* <FooterOrder /> */}
+          <FooterCompleted handleFinish={handlePresentModalPress} />
+        </Footer>
+      </Container>
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={0}
+        topInset={40}
+        snapPoints={snapSheetModalPoints}
+        onChange={handlePresentModalPress}>
+        <ModalFeedback />
+      </BottomSheetModal>
+    </BottomSheetModalProvider>
   );
 }
 
