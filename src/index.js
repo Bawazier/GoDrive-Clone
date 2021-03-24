6bf18e7abcd4f3935e5af0bf99ee4b6522389db8
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-03-24 14:28:31 +0700

    add redux store provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import {Provider} from 'react-redux';
+import store from './redux/store';
+
 import Screens from './navigations';
 
-const App = () => <Screens />;
+const App = () => (
+  <Provider store={store}>
+    <Screens />
+  </Provider>
+);
 
 export default App;
